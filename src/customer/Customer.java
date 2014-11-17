3816fe8766b886ceee6905815f02e135118b51a7
Administrator <Administrator@PC2012081216cnj>
2014-11-17 20:17:56 +0700

    second edition by archi

--- src/customer/Customer.java
+++ src/customer/Customer.java
@@ -14,12 +14,14 @@ import java.util.Scanner;
 public class Customer {
 
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter name : ");
+        System.out.print("Enter your name : ");
         String name = sc.next();
-        System.out.print("Enter tel. number : ");
+        System.out.print("Enter your ID : ");
+        String id = sc.next();
+        System.out.print("Enter your tel. number : ");
         String tel = sc.next();
-        Clerk clerk = new Clerk(name, tel);
+        Clerk clerk = new Clerk(name, id, tel);
     }
 }
