55ac950a006c47d11461b6069b249a7e242b018b
Administrator <Administrator@PC2012081216cnj>
2014-11-17 20:17:45 +0700

    second edition by archi

--- src/customer/Clerk.java
+++ src/customer/Clerk.java
@@ -13,11 +13,13 @@ public class Clerk {
 
     // field
     protected String name;
+    protected String id;
     protected String tel;
 
     //constructor
-    Clerk(String name, String tel) {
+    Clerk(String name, String id, String tel) {
         this.setName(name);
+        this.setID(id);
         this.setTel(tel);
     }
 
@@ -25,9 +27,17 @@ public class Clerk {
     public void setName(String name) {
         this.name = name;
     }
-
+    
     public String getName() {
         return name;
+    }
+    
+    public void setID(String id){
+        this.id = id;
+    }
+    
+    public String getID(String id){
+        return id;
     }
 
     public void setTel(String tel) {
